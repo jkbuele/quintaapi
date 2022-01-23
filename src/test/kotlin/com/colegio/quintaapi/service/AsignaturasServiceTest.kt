bb407a6a929450ef8c09024fbfdcbaa1bc33e2a0
jkbuele <83775377+jkbuele@users.noreply.github.com>
2022-01-23 12:47:07 -0500

    S14 - Pruebas unitarias para las clases service

---
+++ src/test/kotlin/com/colegio/quintaapi/service/AsignaturasServiceTest.kt
@@ -0,0 +1,65 @@
+package com.colegio.quintaapi.service
+
+import com.colegio.quintaapi.model.Asignaturas
+
+import com.colegio.quintaapi.repository.AsignaturasRepository
+import com.google.gson.Gson
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
+
+
+@SpringBootTest
+class AsignaturasServiceTest {
+
+    @InjectMocks
+    lateinit var asignaturasService: AsignaturasService
+
+    @Mock
+    lateinit var asignaturasRepository: AsignaturasRepository
+
+    val returnObject: Asignaturas = Asignaturas().apply {
+        id = 1
+        descricion = "matematicas"
+    }
+    val newObject: Asignaturas = Asignaturas().apply {
+        id = 1
+        descricion = "matematicas"
+    }
+
+    @Test
+    fun saveIsCorrect() {
+        Mockito.`when`(asignaturasRepository.save(Mockito.any(Asignaturas::class.java))).thenReturn(returnObject)
+        val response = asignaturasService.save(newObject)
+        Assertions.assertEquals(response.id, newObject.id)
+        Assertions.assertEquals(response.descricion, newObject.descricion)
+
+    }
+    val jsonString = File("./src/test/resources/asignaturas/crearAsignaturas.json").readText(Charsets.UTF_8)
+    val asignaturasMock = Gson().fromJson(jsonString, Asignaturas::class.java)
+
+    @Test
+    fun saveAsignaturas() {
+        Mockito.`when`(asignaturasRepository.save(Mockito.any(Asignaturas::class.java))).thenReturn(asignaturasMock)
+        val response = asignaturasService.save(asignaturasMock)
+        Assertions.assertEquals(response.id, asignaturasMock.id)
+        Assertions.assertEquals(response.descricion, asignaturasMock.descricion)
+
+    }
+    @Test
+    fun saveAsignaturasFailedDescricion(){
+        Assertions.assertThrows(Exception::class.java) {
+            asignaturasMock.apply { descricion ="    "}
+            Mockito.`when`(asignaturasRepository.save(Mockito.any(Asignaturas::class.java))).thenReturn(asignaturasMock)
+            asignaturasService.save(asignaturasMock)
+        }}
+
+
+
+
+}
---
+++ src/test/kotlin/com/colegio/quintaapi/service/EstudianteServiceTest.kt
@@ -0,0 +1,73 @@
+package com.colegio.quintaapi.service
+
+
+import com.colegio.quintaapi.model.Estudiante
+import com.colegio.quintaapi.repository.EstudianteRepository
+import com.google.gson.Gson
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.springframework.boot.test.context.SpringBootTest
+import java.io.File
+
+
+@SpringBootTest
+class EstudianteServiceTest {
+
+    @InjectMocks
+    lateinit var estudianteService: EstudianteService
+
+    @Mock
+    lateinit var estudianteRepository: EstudianteRepository
+
+    val returnObject: Estudiante = Estudiante().apply {
+        id = 1
+        nombreE = "alex"
+        apellido = "bueno"
+
+    }
+    val newObject: Estudiante = Estudiante().apply {
+        id = 1
+        nombreE = "alex"
+        apellido = "bueno"
+    }
+    @Test
+    fun saveIsCorrect() {
+        Mockito.`when`(estudianteRepository.save(Mockito.any(Estudiante::class.java))).thenReturn(returnObject)
+        val response = estudianteService.save(newObject)
+        Assertions.assertEquals(response.id, newObject.id)
+        Assertions.assertEquals(response.nombreE, newObject.nombreE)
+        Assertions.assertEquals(response.apellido, newObject.apellido)
+    }
+
+    val jsonString = File("./src/test/resources/estudiante/crearEstudiante.json").readText(Charsets.UTF_8)
+    val estudianteMock = Gson().fromJson(jsonString, Estudiante::class.java)
+
+    @Test
+    fun saveEstudiante() {
+        Mockito.`when`(estudianteRepository.save(Mockito.any(Estudiante::class.java))).thenReturn(estudianteMock)
+        val response = estudianteService.save(estudianteMock)
+        Assertions.assertEquals(response.id, estudianteMock.id)
+        Assertions.assertEquals(response.nombreE, estudianteMock.nombreE)
+        Assertions.assertEquals(response.apellido, estudianteMock.apellido)
+    }
+
+
+    @Test
+    fun saveEstudianteFailedNombreE(){
+        Assertions.assertThrows(Exception::class.java) {
+            estudianteMock.apply { nombreE ="    "}
+            Mockito.`when`(estudianteRepository.save(Mockito.any(Estudiante::class.java))).thenReturn(estudianteMock)
+            estudianteService.save(estudianteMock)
+        }}
+
+    @Test
+    fun saveEstudianteFailedApellido(){
+        Assertions.assertThrows(Exception::class.java) {
+            estudianteMock.apply { apellido ="    "}
+            Mockito.`when`(estudianteRepository.save(Mockito.any(Estudiante::class.java))).thenReturn(estudianteMock)
+            estudianteService.save(estudianteMock)
+        }}
+}
